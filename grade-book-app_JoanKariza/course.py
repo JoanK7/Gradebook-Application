d20bafc72b4ebb4fa1f661b1ce8875b38360979a
JoanK7 <j.kariza@alustudent.com>
2024-07-24 11:38:57 -0700

    creating course class

--- grade-book-app_JoanKariza/course.py
+++ grade-book-app_JoanKariza/course.py
@@ -1,8 +1,23 @@
 #!/usr/bin/python3
-class Course:
-    """Class to represent a course."""
+import csv
 
+class Course:
     def __init__(self, name, trimester, credits):
         self.name = name
         self.trimester = trimester
         self.credits = credits
+
+    def save_to_csv(self):
+        with open('courses.csv', 'a', newline='') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow([self.name, self.trimester, self.credits])
+
+    @staticmethod
+    def get_course_credits(course_name):
+        with open('courses.csv', 'r') as csvfile:
+            reader = csv.reader(csvfile)
+            for row in reader:
+                if row[0] == course_name:
+                    return float(row[2])
+        return 0
+
