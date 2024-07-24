3fed3d94c9e34619b721bca3041e61076986d25b
JoanK7 <j.kariza@alustudent.com>
2024-07-24 11:37:33 -0700

    creating student class

--- grade-book-app_JoanKariza/student.py
+++ grade-book-app_JoanKariza/student.py
@@ -1,26 +1,12 @@
 #!/usr/bin/python3
+import csv
+
 class Student:
-    """Class to represent a student."""
-
-    def __init__(self, email, names):
+    def __init__(self, email, name):
         self.email = email
-        self.names = names
-        self.courses_registered = {}
-        self.GPA = 0.0
+        self.name = name
 
-    def calculate_GPA(self):
-        """Calculate the GPA of the student based on registered courses."""
-        if not self.courses_registered:
-            self.GPA = 0.0
-            return
-        total_credits = 0
-        total_points = 0
-        for course, grade in self.courses_registered.items():
-            total_credits += course.credits
-            total_points += grade * course.credits
-        self.GPA = total_points / total_credits
-
-    def register_for_course(self, course, grade):
-        """Register a student for a course with the given grade."""
-        self.courses_registered[course] = grade
-        self.calculate_GPA()
+    def save_to_csv(self):
+        with open('students.csv', 'a', newline='') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow([self.email, self.name])
